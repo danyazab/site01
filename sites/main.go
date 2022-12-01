7500e17e0c6cbaae13f8ea1382649793e656096d
denial <zabolotnijdanilo@gmail.com>
2022-12-01 16:49:11 +0200

    .

--- sites/main.go
+++ sites/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	_ "database/sql"
+	"github.com/labstack/echo/v4"
 	"net/http"
 	"strconv"
-
-	"github.com/labstack/echo/v4"
 )
 
 func main() {
@@ -21,35 +21,57 @@ func main() {
 	e.DELETE("/my-db/:dbName", deleteDb)
 
 	e.Logger.Fatal(e.Start(":8080"))
+
 }
+
+type dbUser map[string]*user
+type db map[string]*base
+
+var (
+	DbU dbUser = make(map[string]*user)
+
+	DbS db = make(map[string]*base)
+)
 
 type (
 	user struct {
-		KEY  interface{} `json:"key"`
-		Name interface{} `json:"name"`
+		KEY  string `json:"key"`
+		Name string `json:"name"`
+	}
+	base struct {
+		dbName string
+		Vmist  interface{}
 	}
 )
 
-var (
-	users = map[interface{}]*user{}
-)
+var perR interface{}
 
 func createUser(c echo.Context) error {
+	//per1 := c.QueryParam("dbName")
 	per := c.QueryParam("key")
+	perR = c.QueryParam("dbName")
+
+	//var x user
 	u := &user{
+		//DbName: per1,
 		KEY: per,
 	}
+
+	//DbU["key"] = &user{
+	//	//DbName: per1,
+	//	KEY: per,
+	//}
 
 	if err := c.Bind(u); err != nil {
 		return err
 	}
-	users[c.Param("key")] = u
+	DbU[c.Param("key")] = u
 
 	return c.JSON(http.StatusCreated, u)
 }
 
 func getUser(c echo.Context) error {
-	res, err := users[c.Param("key")]
+	res, err := DbU[c.Param("key")]
 	if !err {
 		return c.String(http.StatusNotFound, "There is no such key")
 	}
@@ -61,39 +83,56 @@ func updateUser(c echo.Context) error {
 	if err := c.Bind(u); err != nil {
 		return err
 	}
-	users[c.Param("key")].Name = u.Name
+	DbU[c.Param("key")].Name = u.Name
 	return c.JSON(http.StatusOK, u.Name)
 }
 
 func deleteUser(c echo.Context) error {
 	key, _ := strconv.Atoi(c.Param("key"))
-	delete(users, key)
+	delete(DbU, string(key))
 	return c.NoContent(http.StatusNoContent)
 }
 
-//func allUsers(c echo.Context) error {
-
-//return c.JSON(http.StatusOK, &aif)
-//}
-
 func createDb(c echo.Context) error {
+	//un := new(base)
+	per := c.QueryParam("dbName")
+	//ne := new(db)
+	//gra := user{
+	//	KEY:  un.KEY,
+	//	Name: un.Name}
 
-	//c.Param(("Db") := new(map[interface{}]*user)
+	u := &base{
+		dbName: per,
+		Vmist:  DbU,
+	}
+	//DbS["dbName"] = &base{
+	//	dbName: per,
+	//	Vmist:  ne,
+	//}
 
-	return c.JSON(http.StatusCreated, "")
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	DbS[c.Param("dbName")] = u
+
+	return c.JSON(http.StatusCreated, u)
 }
 func getDb(c echo.Context) error {
 
-	return c.JSON(http.StatusOK, "")
-
+	res, err := DbS[c.Param("dbName")]
+	if !err {
+		return c.String(http.StatusNotFound, "There is no such key")
+	}
+	return c.JSON(http.StatusOK, res)
 }
+
 func listDb(c echo.Context) error {
 
-	return c.JSON(http.StatusOK, "")
+	return c.JSON(http.StatusOK, DbS)
 
 }
 func deleteDb(c echo.Context) error {
-
+	key, _ := strconv.Atoi(c.Param("dbName"))
+	delete(DbS, string(key))
 	return c.NoContent(http.StatusNoContent)
-
 }
